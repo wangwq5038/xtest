0d61816a9156b76696886d157e0f7e8692e3ea67
wangwq5038 <wangwq5038@163.com>
2020-08-04 23:40:33 +0800

    修改demo03中的test_01

--- demo02.py
+++ demo02.py
@@ -6,3 +6,8 @@
 @LastEditors: wangwq
 @LastEditTime: 2020-06-27 16:29:15
 '''
+
+
+import time
+
+print(time.ctime())
--- demo03.py/test_03.py
+++ demo03.py/test_03.py
@@ -1,1 +1,6 @@
 print('hello world')
+
+
+import time
+#打印当前时间
+print(time.ctime())
